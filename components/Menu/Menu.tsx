11c71befaefbf9bbde615b62381e5c8e56ea8027
tomatoCultivator <lpanahon06@gmail.com>
2025-10-19 03:42:05 +0800

    feature(signout): added a signout feature in sidebar, also i update some minor hover effect in side bar menu

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -12,6 +12,7 @@ import { FaUserCircle } from "react-icons/fa";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 
 import { useState } from 'react';
+import { signOut } from 'next-auth/react';
 const Menu = () => {
   const [open, setOpen] = useState(true)
   const [parentOpen, setParentOpen] = useState(true)
@@ -29,11 +30,11 @@ const Menu = () => {
       </div>
       <div className={`flex flex-col justify-between gap-10 ${open ? 'p-5' : 'p-0 py-5 justify-center items-center text-[20px]'}`}>
         <div className='flex flex-col gap-5'>
-          <div className='flex gap-2 justify-start items-center '>
-            <MdDashboard className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px]'}`} />
-            {open && <Label>Dashboard</Label>}
+          <div className='flex gap-2 justify-start items-center cursor-pointer '>
+            <MdDashboard className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px] hover:bg-blue-800'}`} />
+            {open && <Label className='cursor-pointer'>Dashboard</Label>}
           </div>
-          <div className='flex gap-2 justify-start items-center'>
+          <div className='flex gap-2 justify-start items-center '>
 
             <Accordion
               type="single"
@@ -43,10 +44,10 @@ const Menu = () => {
             >
               <AccordionItem value="item-1">
                 <div className='flex gap-2 justify-start items-center delay-100'>
-                  <GoPackage className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px]'}`} />
-                  {open && <AccordionTrigger className='delay-200 duration-200'>Products</AccordionTrigger>}
+                  <GoPackage className={`p-1 text-[30px] rounded cursor-pointer ${!open && 'border border-white/15 text-[35px] hover:bg-blue-800'}`} />
+                  {open && <AccordionTrigger className='delay-200 duration-200 cursor-pointer'>Products</AccordionTrigger>}
                 </div>
-                {open && <AccordionContent className="flex flex-col gap-3 text-balance text-white py-2 px-6">
+                {open && <AccordionContent className="flex flex-col gap-3 text-balance text-white py-2 px-6 cursor-pointer">
 
                   <Label className=''>Computers</Label>
                   <Label className=''>Components</Label>
@@ -58,20 +59,19 @@ const Menu = () => {
               </AccordionItem>
             </Accordion>
           </div>
-          <div className='flex gap-2 justify-start items-center '>
-            <TbTruckDelivery className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px]'}`} />
-            {open && <Label>Orders</Label>}
+          <div className='flex gap-2 justify-start items-center cursor-pointer'>
+            <TbTruckDelivery className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px] hover:bg-blue-800'}`} />
+            {open && <Label className='cursor-pointer'>Orders</Label>}
           </div>
-          <div className='flex gap-2 justify-start items-center'>
-            <IoIosSettings className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px]'}`} />
-            {open && <Label>Settings</Label>}
+          <div className='flex gap-2 justify-start items-center cursor-pointer'>
+            <IoIosSettings className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px] hover:bg-blue-800'}`} />
+            {open && <Label className='cursor-pointer'>Settings</Label>}
           </div>
         </div>
-        <div className='flex items-center gap-2 justify-start'>
-          <FaUserCircle className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px]'}`} />
-          {open && <Label>Signout</Label>}
-        </div>
-
+        <button onClick={() => signOut()} className='flex items-center gap-2 justify-start cursor-pointer'>
+          <FaUserCircle className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px] hover:bg-blue-800'}`} />
+          {open && <label className='cursor-pointer'>Signout</label>}
+        </button>
       </div>
 
 
