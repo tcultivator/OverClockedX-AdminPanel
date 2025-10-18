74ed606afcf8ac4e3d3a4bf95eaee211c3fd8a96
tomatoCultivator <lpanahon06@gmail.com>
2025-10-19 03:04:21 +0800

    feature(side menu bar): added a custom side menu bar, this has an action that can hide and show the menu bar, if the menu bar is hide only icons is showing, and if in show, this custom side menu bar needed some code factoring and optimize, but the logic is set

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,9 +1,80 @@
+"use client"
 import React from 'react'
+import { Label } from '../ui/label'
+//icons
+import { FiSidebar } from "react-icons/fi";
+import { MdDashboard } from "react-icons/md";
+import { GoPackage } from "react-icons/go";
+import { TbTruckDelivery } from "react-icons/tb";
+import { IoIosSettings } from "react-icons/io";
+import { FaUserCircle } from "react-icons/fa";
 
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
+
+import { useState } from 'react';
 const Menu = () => {
+  const [open, setOpen] = useState(true)
+  const [parentOpen, setParentOpen] = useState(true)
+  const menuBarToggle = () => {
+    setParentOpen(prev => !prev)
+    setTimeout(() => {
+      setOpen(prev => !prev)
+    }, 100);
+  }
   return (
-    <div className='w-[400px] bg-white/30 text-black rounded[10px] p-2 h-screen'>
-      this is menu
+    <div className={`${parentOpen ? 'w-[250px]' : 'w-[50px]'} duration-200 bg-[#171717] border-r border-white/15 text-white rounded[10px] h-screen`}>
+      <div className={`flex ${open ? 'justify-between  p-5' : 'justify-center  p-2 text-[25px] text-center'} items-center border-b border-white/15`}>
+        {open && <Label>OverClockedX</Label>}
+        <button className='cursor-pointer' onClick={menuBarToggle}><FiSidebar /></button>
+      </div>
+      <div className={`flex flex-col justify-between gap-10 ${open ? 'p-5' : 'p-0 py-5 justify-center items-center text-[20px]'}`}>
+        <div className='flex flex-col gap-5'>
+          <div className='flex gap-2 justify-start items-center '>
+            <MdDashboard className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px]'}`} />
+            {open && <Label>Dashboard</Label>}
+          </div>
+          <div className='flex gap-2 justify-start items-center'>
+
+            <Accordion
+              type="single"
+              collapsible
+              className="w-full"
+              defaultValue=""
+            >
+              <AccordionItem value="item-1">
+                <div className='flex gap-2 justify-start items-center delay-100'>
+                  <GoPackage className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px]'}`} />
+                  {open && <AccordionTrigger className='delay-200 duration-200'>Products</AccordionTrigger>}
+                </div>
+                {open && <AccordionContent className="flex flex-col gap-3 text-balance text-white py-2 px-6">
+
+                  <Label className=''>Computers</Label>
+                  <Label className=''>Components</Label>
+                  <Label className=''>Peripherals</Label>
+                  <Label className=''>Networks</Label>
+
+
+                </AccordionContent>}
+              </AccordionItem>
+            </Accordion>
+          </div>
+          <div className='flex gap-2 justify-start items-center '>
+            <TbTruckDelivery className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px]'}`} />
+            {open && <Label>Orders</Label>}
+          </div>
+          <div className='flex gap-2 justify-start items-center'>
+            <IoIosSettings className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px]'}`} />
+            {open && <Label>Settings</Label>}
+          </div>
+        </div>
+        <div className='flex items-center gap-2 justify-start'>
+          <FaUserCircle className={`p-1 text-[30px] rounded ${!open && 'border border-white/15 text-[35px]'}`} />
+          {open && <Label>Signout</Label>}
+        </div>
+
+      </div>
+
+
     </div>
   )
 }
