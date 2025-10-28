ebc55dc8ddac0cf8d37a1c89f4357559943ffc04
tomatoCultivator <lpanahon06@gmail.com>
2025-10-28 18:58:56 +0800

    update(uiOfSuspense): added a shadcn skeleton loading, while loading the inventory page

--- app/LandingPage/ProductPage/page.tsx
+++ app/LandingPage/ProductPage/page.tsx
@@ -3,10 +3,20 @@ import React from 'react'
 import Header from './components/Header/Header'
 import ProductList from './components/ProductsList/ProductList'
 import { Suspense } from 'react'
+import { Label } from '@/components/ui/label';
+import { Skeleton } from "@/components/ui/skeleton"
 const ProductPage = () => {
     return (
         <div className='flex w-full flex-col gap-2'>
-            <Suspense fallback={<>waiting...</>}>
+            <Suspense fallback={
+                <>
+                    <div className="w-full h-full">
+                        <div className="flex flex-col gap-2 h-[98vh]">
+                            <Skeleton className="h-[10%] w-full p-1 shadow-sm bg-white/60 rounded-[15px]" />    
+                            <Skeleton className="h-[90%] w-full p-1 shadow-sm bg-white/60 rounded-[15px]" />
+                        </div>
+                    </div>
+                </>}>
                 <Header />
                 <div className='flex gap-2 w-full'>
                     <ProductList />
