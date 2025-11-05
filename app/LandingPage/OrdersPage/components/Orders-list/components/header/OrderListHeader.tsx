1aa7d7eb890f1f573ef11bc03359376ecc17e491
tomatoCultivator <lpanahon06@gmail.com>
2025-11-06 05:19:38 +0800

    update(search UI): added a loading spinner to search button, the temp data taht hold previous orders data is only store the previous data if that temp data has no value , earlier it has bug to that, whats happening is if i search, the previous data is going to temp data, then if i search again, it still going to temp data, now the first orders data is gone, it replace of result of the first search, now i use condition that it only going to temp data if the temp data has no value, now even if i search multiple time without clear search, the first previous orders data is still there, i also added a spinner loading with label searching next to input, so admin can notice that its search, i added that because the search button is quite small and the loading spinner to that is not noticable

--- app/LandingPage/OrdersPage/components/Orders-list/components/header/OrderListHeader.tsx
+++ app/LandingPage/OrdersPage/components/Orders-list/components/header/OrderListHeader.tsx
@@ -9,16 +9,17 @@ import { useOrderStore } from '@/stores/ordersStore';
 import { GroupOrdersData } from '@/utils/GroupOrderData';
 import { GroupedOrder } from '@/types/GroupDataType';
 import { IoCloseCircleSharp } from "react-icons/io5";
-
+import { ClipLoader } from 'react-spinners';
 const OrderListHeader = () => {
     const [searchInput, setSearchInput] = useState('')
     const setOrders_data = useOrderStore((state) => state.setOrders_data)
     const [tempOrdersData, setTempOrdersData] = useState<GroupedOrder[]>([])
     const orders_data = useOrderStore((state) => state.orders_data)
+    const [loading, setLoading] = useState(false)
     const Submit_Search = async () => {
         console.log(searchInput)
         if (searchInput == '') return
-        setTempOrdersData(orders_data)
+        setLoading(true)
         const searchRequest = await fetch('/api/OrdersPage/Search', {
             method: 'POST',
             headers: {
@@ -28,8 +29,11 @@ const OrderListHeader = () => {
         })
         const searchRequest_result = await searchRequest.json()
         if (searchRequest_result.status == 500) return
-
+        if (tempOrdersData.length == 0) {
+            setTempOrdersData(orders_data)
+        }
         setOrders_data(GroupOrdersData(searchRequest_result))// this is the reusable function that turn raw data to group data
+        setLoading(false)
     }
 
     const clearSearch = () => {
@@ -41,9 +45,13 @@ const OrderListHeader = () => {
         <div className='w-full p-3 px-5 border-b flex justify-between items-center'>
             <Label className="text-[15px] font-semibold">Orders</Label>
             <div className='flex items-center gap-2'>
-                {tempOrdersData.length > 0 && < Button onClick={clearSearch} className='text-black/50 focus:bg-transparent cursor-pointer' variant={'outline'}><IoCloseCircleSharp className='text-red-400' />clear search</Button>}
-                <Input value={searchInput || ''} type='text' placeholder='Reference_id/Email' onChange={(e) => setSearchInput(e.target.value)} className='w-[300px]'/>
-                <Button onClick={Submit_Search} variant={'secondary'}><CiSearch /></Button>
+
+                {
+                    loading ? <Label className='text-black/50 flex items-center'><ClipLoader size={15} color='black/50' />Searching...</Label> :
+
+                        tempOrdersData.length > 0 && < Button onClick={clearSearch} className='text-black/50 focus:bg-transparent cursor-pointer' variant={'outline'}><IoCloseCircleSharp className='text-red-400' />clear search</Button>}
+                <Input value={searchInput || ''} type='text' placeholder='Reference_id/Email' onChange={(e) => setSearchInput(e.target.value)} className='w-[350px] p-5' />
+                <Button disabled={loading} onClick={Submit_Search} variant={'default'} className='cursor-pointer h-[40px] w-[40px] p-0'>{loading ? <ClipLoader  size={15} color='white' /> : <CiSearch />}</Button>
             </div>
         </div >
     )
