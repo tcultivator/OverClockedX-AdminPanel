df56e3393c45e1da07327d555b7018f28dfe3547
tcultivator <whypanahon06@gmail.com>
2025-11-19 13:06:57 +0800

    Update route.ts

--- app/api/Dashboard/popular-product/route.ts
+++ app/api/Dashboard/popular-product/route.ts
@@ -15,20 +15,31 @@ export async function GET(req: NextRequest) {
     try {
         const year = Number(req.nextUrl.searchParams.get("year"))
         const month = Number(req.nextUrl.searchParams.get("month"))
-        const query = `SELECT order_items.product_id,
-        products.product_name,
-        products.product_image,
-        products.price,
-        products.stocks,
-        products.base_stocks,
-        products.sales_count, 
-        COUNT(*) AS total_orders 
-        FROM order_items 
-        JOIN products ON products.product_id=order_items.product_id 
-        WHERE MONTH(order_items.created_at) = ? 
-        AND YEAR(order_items.created_at) = ? 
-        GROUP BY product_id 
-        ORDER BY total_orders DESC LIMIT 1;
+        const query = `SELECT 
+    order_items.product_id,
+    products.product_name,
+    products.product_image,
+    products.price,
+    products.stocks,
+    products.base_stocks,
+    products.sales_count,
+    COUNT(*) AS total_orders
+FROM order_items
+JOIN products 
+    ON products.product_id = order_items.product_id
+WHERE MONTH(order_items.created_at) = 11
+  AND YEAR(order_items.created_at) = 2025
+GROUP BY 
+    order_items.product_id,
+    products.product_name,
+    products.product_image,
+    products.price,
+    products.stocks,
+    products.base_stocks,
+    products.sales_count
+ORDER BY total_orders DESC
+LIMIT 1;
+
 `
         const [rows] = await db.query(query, [month, year])
         const popular_product_result = rows as popularProducts[]
