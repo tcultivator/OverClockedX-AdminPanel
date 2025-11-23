8b7ea9f0e65a94e83092873e567f5439685615b4
tcultivator <whypanahon06@gmail.com>
2025-11-24 01:37:58 +0800

    Update route.ts

--- app/api/addProducts/route.ts
+++ app/api/addProducts/route.ts
@@ -8,8 +8,8 @@ type subscriber_email = {
 export async function POST(req: NextRequest) {
     try {
         const body = await req.json()
-        const query = 'INSERT INTO products (product_id ,category,parent,product_name,product_image,price,stocks,description,brand,sales_count) VALUES (?,?,?,?,?,?,?,?,?,?)'
-        await db.query(query, [body.data.product_id, body.data.category, body.data.parent, body.data.product_name, body.data.product_image, body.data.price, body.data.stocks, body.data.description, body.data.brand, 0])
+        const query = 'INSERT INTO products (product_id ,category,parent,product_name,product_image,price,stocks,base_stocks,description,brand,sales_count) VALUES (?,?,?,?,?,?,?,?,?,?,?)'
+        await db.query(query, [body.data.product_id, body.data.category, body.data.parent, body.data.product_name, body.data.product_image, body.data.price, body.data.stocks,body.data.stocks, body.data.description, body.data.brand, 0])
 
         const [rows] = await db.query('SELECT email FROM subscribe_users')
         const emails = rows as subscriber_email[]
